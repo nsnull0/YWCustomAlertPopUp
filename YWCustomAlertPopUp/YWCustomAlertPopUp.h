da174006d1cc955e4108ffa8757775ebf4e87b92
System Administrator <root@Yosephs-MacBook-Pro.local>
2017-09-06 12:13:41 +0900

    initial commit

---
+++ YWCustomAlertPopUp/YWCustomAlertPopUp.h
@@ -0,0 +1,19 @@
+//
+//  YWCustomAlertPopUp.h
+//  YWCustomAlertPopUp
+//
+//  Created by Yoseph Wijaya on 9/6/17.
+//  Copyright © 2017 meself. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YWCustomAlertPopUp.
+FOUNDATION_EXPORT double YWCustomAlertPopUpVersionNumber;
+
+//! Project version string for YWCustomAlertPopUp.
+FOUNDATION_EXPORT const unsigned char YWCustomAlertPopUpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YWCustomAlertPopUp/PublicHeader.h>
+
+
